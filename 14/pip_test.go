d370bcb1723b25f3422cafe0802b20aacfffc3dc
hideaki10 <senshikou1985@gmail.com>
2022-09-05 23:32:22 +0900

    fix 13demo

--- 14/pip_test.go
+++ 14/pip_test.go
@@ -2,6 +2,8 @@ package pipeline
 
 import (
 	"bytes"
+	"errors"
+	"io"
 
 	"testing"
 
@@ -10,8 +12,8 @@ import (
 
 func TestStdout(t *testing.T) {
 	t.Parallel()
-	want := "Hello, World\n"
-	p := FromOfString(want)
+	p := FromOfString("Hello, World\n")
+	p.Error = errors.New("oh no")
 	buf := &bytes.Buffer{}
 
 	p.Output = buf
@@ -22,7 +24,101 @@ func TestStdout(t *testing.T) {
 	}
 
 	got := buf.String()
+	// if !cmp.Equal(want, got) {
+	// 	t.Errorf("want %q, got %q", want, got)
+	// }
+
+	if got != "" {
+		t.Errorf("want no output from Stdout after error, but got %q", got)
+	}
+}
+
+func TestFromFile(t *testing.T) {
+	t.Parallel()
+	want := []byte("Hello, world\n")
+	p := FromFile("testdata/hello.txt")
+
+	if p.Error != nil {
+		t.Fatal(p.Error)
+	}
+
+	got, err := io.ReadAll(p.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	if !cmp.Equal(want, got) {
 		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func TestFromFileInvalid(t *testing.T) {
+	t.Parallel()
+	p := FromFile("doesnt-exist.txt")
+	if p.Error == nil {
+		t.Fatal("want error opening non-existent file but got nil")
+	}
+}
+
+func TestString(t *testing.T) {
+	t.Parallel()
+	want := "Hello World\n"
+	p := FromOfString(want)
+	got, err := p.String()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !cmp.Equal(want, got) {
+		t.Errorf("want %q , got %q", want, got)
+	}
+
+}
+
+func TestStringError(t *testing.T) {
+	t.Parallel()
+	p := FromOfString("Hello World\n")
+	p.Error = errors.New("oh no")
+	_, err := p.String()
+	if err == nil {
+		t.Error("want error from String when pipeline has error, but got nil")
+	}
+}
+
+func TestColumn(t *testing.T) {
+	t.Parallel()
+	input := "1 2 3\n1 2 3\n 1 2 3\n"
+
+	p := FromOfString(input)
+	want := "2\n 2\n 2\n"
+	got, err := p.Column(2).String()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestColumnError(t *testing.T) {
+	t.Parallel()
+	p := FromOfString("1 2 3\n")
+	p.Error = errors.New("oh no")
+	data, err := io.ReadAll(p.Column(1).Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(data) > 0 {
+		t.Errorf("want no output from Column after error, but got %q", data)
+	}
+}
+
+func TestColumnInvalid(t *testing.T) {
+	t.Parallel()
+	p := FromOfString("")
+	p.Column(-1)
+	if p.Error == nil {
+		t.Error("want error on non-positive Column, but got nil")
 	}
 }
--- 14/pipeline.go
+++ 14/pipeline.go
@@ -1,7 +1,11 @@
 package pipeline
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"io"
+	"os"
 	"strings"
 )
 
@@ -18,5 +22,55 @@ func FromOfString(s string) *Pipeline {
 }
 
 func (p *Pipeline) Stdout() {
+
+	if p.Error != nil {
+		return
+	}
 	io.Copy(p.Output, p.Reader)
+
+}
+
+func FromFile(pathname string) *Pipeline {
+	f, err := os.Open(pathname)
+	if err != nil {
+		return &Pipeline{Error: err}
+	}
+	return &Pipeline{Reader: f}
+}
+
+func (p *Pipeline) String() (string, error) {
+	if p.Error != nil {
+		return "", p.Error
+	}
+
+	data, err := io.ReadAll(p.Reader)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
+
+func (p *Pipeline) Column(col int) *Pipeline {
+
+	if p.Error != nil {
+		p.Reader = strings.NewReader("")
+		return p
+	}
+	if col < 1 {
+		p.Error = fmt.Errorf("bad column %d: must be positive", col)
+	}
+
+	result := &bytes.Buffer{}
+	scanner := bufio.NewScanner(result)
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		if len(fields) < col {
+			continue
+		}
+		fmt.Fprintln(result, fields[col-1])
+	}
+	return &Pipeline{
+		Reader: result,
+	}
 }
